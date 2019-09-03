fb7cbea339220976f58d753da0f2820fc54db6c9
byfaith <zlu40@qq.com>
2019-09-03 21:54:40 +0800

    first commit

---
+++ src/main/java/com/demo/forum/controller/AuthorizeController.java
@@ -0,0 +1,67 @@
+package com.demo.forum.controller;
+
+import com.demo.forum.dto.AccessTokenDTO;
+import com.demo.forum.dto.GithubUser;
+import com.demo.forum.mapper.UserMapper;
+import com.demo.forum.model.User;
+import com.demo.forum.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request,
+                           HttpServletResponse response){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        System.out.println(githubUser.getName());
+        if(githubUser != null){
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            request.getSession().setAttribute("user",githubUser);
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
+
+            return "redirect:/" ;
+        }else{
+            return "redirect:/" ;
+        }
+    }
+}
---
+++ src/main/java/com/demo/forum/mapper/QuestionMapper.java
@@ -0,0 +1,4 @@
+package com.demo.forum.mapper;
+
+public interface QuestionMapper {
+}
